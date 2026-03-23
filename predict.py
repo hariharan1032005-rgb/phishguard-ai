d4a6833d3578a8a2d310a23d3cb9c12f87d61407
Hariharan103 <hariharan1032005@gmail.com>
2026-03-23 19:05:38 +0530

    Implement error handling in predict_url function
    Added error handling to the predict_url function to return default values in case of exceptions.

--- predict.py
+++ predict.py
@@ -1,14 +1,17 @@
 import joblib
 from feature_extraction import extract_features
 
-model = joblib.load("phishguard_model.pkl")
+# Load model
+model = joblib.load("model.pkl")   # 🔥 make sure file name matches
 
 def predict_url(url):
+    try:
+        features = extract_features(url)
 
-    features = extract_features(url)
+        prediction = model.predict([features])[0]
+        probability = model.predict_proba([features])[0][1]
 
-    prediction = model.predict([features])[0]
+        return prediction, probability, features
 
-    probability = model.predict_proba([features])[0][1]
-
-    return prediction, probability, features
+    except Exception as e:
+        return 0, 0.0, [0,0,0,0,0,0]
